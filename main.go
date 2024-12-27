dc621d173951a19628ea4a95fcdbd930323cd882
John <128350391+alpaOo@users.noreply.github.com>
2024-12-27 23:54:11 +0800

    Add command argument handling and error checking in main

--- main.go
+++ main.go
@@ -2,27 +2,61 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/johndosdos/blog_aggregator/internal/commands"
 	"github.com/johndosdos/blog_aggregator/internal/config"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("missing argument. e.g. <command> [arguments]")
+		os.Exit(1)
+	}
+
 	// Config.Read(Src string)
 	newConfig, err := config.Read(".gatorconfig.json")
 	if err != nil {
-		fmt.Errorf("%w", err)
+		fmt.Println("%w", err)
+		os.Exit(1)
 	}
 
-	// Config.SetUser(Dest, Src string)
-	err = newConfig.SetUser(".gatorconfig.json", "John")
-	if err != nil {
-		fmt.Errorf("%w", err)
+	args := os.Args
+	cmd := commands.Command{
+		// when using os.Args, we need to start at index 1 because index 0 is the program name
+		// index 1 = command name, index 2 = username
+		Name: args[1],
+		Args: args[2:],
 	}
 
-	newConfig, err = config.Read(".gatorconfig.json")
-	if err != nil {
-		fmt.Errorf("%w", err)
+	/* 	// TEST
+	   	cmd := commands.Command{
+	   		// when using os.Args, we need to start at index 1 because index 0 is the program name
+	   		// index 1 = command name, index 2 = username
+	   		Name: "login",
+	   		Args: []string{"jane"},
+	   	} */
+
+	state := &commands.State{Config: &newConfig}
+
+	handlerMap := make(map[string]func(*commands.State, commands.Command) error)
+	cmds := commands.Commands{Handlers: handlerMap}
+	cmds.Register(cmd.Name, commands.HandlerLogin)
+
+	if err := cmds.Run(state, cmd); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	fmt.Println(newConfig)
+	/* 	// Config.SetUser(Dest, Src string)
+	   	err = newConfig.SetUser(".gatorconfig.json", "John")
+	   	if err != nil {
+	   		fmt.Errorf("%w", err)
+	   	}
+
+	   	newConfig, err = config.Read(".gatorconfig.json")
+	   	if err != nil {
+	   		fmt.Errorf("%w", err)
+	   	} */
+
 }
