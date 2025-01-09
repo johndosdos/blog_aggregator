fae304ad5831efcb97ac3db11a1fb00e578ce521
John <128350391+alpaOo@users.noreply.github.com>
2025-01-09 16:23:34 +0800

    feat(db): Add queries to retrive and delete feeds

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -2,3 +2,13 @@
 INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
+
+-- name: GetFeeds :many
+SELECT
+    feeds.*,
+    users.name AS username
+FROM feeds
+JOIN users ON feeds.user_id = users.id;
+
+-- name: DeleteFeeds :exec
+DELETE FROM feeds;
