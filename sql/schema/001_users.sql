1c5f524621a9a82b43b6e8310238d5d51fea2da0
John <128350391+alpaOo@users.noreply.github.com>
2025-01-09 15:33:16 +0800

    chore(db): Move feeds queries to separate files

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -6,17 +6,5 @@ CREATE TABLE users (
     name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE feeds (
-    id UUID PRIMARY KEY NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NULL,
-    url TEXT UNIQUE NOT NULL,
-    user_id UUID NOT NULL,
-    FOREIGN KEY (user_id)
-    REFERENCES users (id)
-    ON DELETE CASCADE
-);
-
 -- +goose Down
 DROP TABLE users;
