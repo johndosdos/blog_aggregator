39c17aaba38742691ce10736a75dbf13797cf48a
John <128350391+alpaOo@users.noreply.github.com>
2025-01-13 20:33:56 +0800

    feat: Add query to delete user feed record (unfollow)

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -26,3 +26,12 @@ WHERE users.name = $1;
 
 -- name: DeleteUsersFeedFollows :exec
 DELETE FROM feed_follows;
+
+-- name: DeleteFeedFollow :exec
+DELETE FROM feed_follows
+WHERE
+    feed_follows.user_id = $1 AND feed_follows.feed_id = (
+        SELECT id 
+        FROM feeds
+        WHERE url = $2
+    );
